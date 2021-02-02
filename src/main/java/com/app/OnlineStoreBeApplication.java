0c9bd555a67d885d25c8c15d0f9a875abc94c779
KonarzewskiP <pawelkonarzewski13@gmail.com>
2021-02-02 14:24:30 +0000

    initial commit

---
+++ src/main/java/com/app/OnlineStoreBeApplication.java
@@ -0,0 +1,13 @@
+package com.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineStoreBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineStoreBeApplication.class, args);
+    }
+
+}
